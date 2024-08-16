7d1a490dc83ee76b4ef6652436cc526f6f94f9e2
leeyeram84 <killhvhl1129@naver.com>
2024-08-16 12:45:55 +0900

    feat : 데이터 받아오기 및 공란 유효성 검사

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { useState } from "react";
 
-const todos = [
+const SAMPLE_TODOS = [
     { id: 1, text: "Buy milk" },
     { id: 2, text: "Clean the house" },
     { id: 3, text: "Go for a run" },
@@ -14,12 +15,49 @@ const todos = [
 ];
 
 const TodoList = () => {
+    const [todos, setTodos] = useState(SAMPLE_TODOS);
+
+    const hadleSubmit = (e) => {
+        e.preventDefault(); // 화면 리로드 방지
+
+        // 공백 유효성 검사 // trim() : 문자열의 양 쪽 공백을 제거하는 함수
+        // trim으로 공백을 제거하여 값이 없는 경우 바로 리턴 => 아무런 행동도 일어나지 않음
+        if (!newTodo.trim()) {
+            return;
+        }
+
+        // 새로 받은 데이터 추가 및 그리기
+        setTodos([...todos, { id: crypto.randomUUID(), text: newTodo }]);
+
+        // input box 초기화
+        setnewTodos("");
+    };
+
+    // 새로운 입력뱓은 값 저장
+    const [newTodo, setnewTodos] = useState("");
+
+    // 새로운 값 입력 받기
+    const handleInputChange = (e) => {
+        setnewTodos(e.target.value);
+    };
+
     return (
-        <ul>
-            {todos.map((todo) => (
-                <li key={todo.id}>{todo.text}</li>
-            ))}
-        </ul>
+        <div>
+            <form onSubmit={hadleSubmit}>
+                <input
+                    type="text"
+                    value={newTodo}
+                    onChange={handleInputChange}
+                    placeholder="Enter a new Todo"
+                ></input>
+                <button type="submit">ADD TODO</button>
+            </form>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.text}</li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
