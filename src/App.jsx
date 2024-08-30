9b3daae7961263284e7b776b1448d258201ede7b
leeyeram84 <killhvhl1129@naver.com>
2024-08-30 12:09:43 +0900

    vite react 초기 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import TodoList from "./components/TodoList";
-import "the-new-css-reset/css/reset.css";
-
 function App() {
-    return (
-        <main className="main-center">
-            <TodoList />
-        </main>
-    );
+    return <></>;
 }
 
 export default App;
