e94e5b3a964ae2d9e49f08f1ab616af406bc8961
leeyeram84 <killhvhl1129@naver.com>
2024-08-30 16:54:26 +0900

    feat : 완료 기능 추가

--- src/componentes/TodoContainer.jsx
+++ src/componentes/TodoContainer.jsx
@@ -30,6 +30,23 @@ const TodoContainer = () => {
         setTodos(filteredTodos);
     };
 
+    const onToggleCompleted = (id) => {
+        const newTodos = todos.map((todo) => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    isCompleted: !todo.isCompleted,
+                };
+            }
+
+            return todo;
+        });
+
+        setTodos(newTodos);
+
+        console.log(newTodos);
+    };
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -43,7 +60,10 @@ const TodoContainer = () => {
             <ul>
                 {todos.map((todo) => (
                     <li key={todo.id}>
-                        {todo.text}-{todo.isCompleted ? "취소" : "미완료"}
+                        {todo.text}-{todo.isCompleted ? "완료" : "미완료"}
+                        <button onClick={() => onToggleCompleted(todo.id)}>
+                            {todo.isCompleted ? "취소" : "완료"}
+                        </button>
                         <button
                             onClick={() => {
                                 onDelete(todo.id);
